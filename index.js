b5208b8fa357a1117f5220e451ad2d7a7f033e74
Egor Ushakov <witluv@icloud.com>
2023-08-09 20:55:49 +0300

    feat: add click function

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ wrapper.append(grid);
 let width = 10;
 let bombCount = 10;
 let squares = [];
+let isGameOver = false;
 
 function createBoard() {
 	const bombArray = Array(bombCount).fill('bomb');
@@ -74,3 +75,38 @@ function createBoard() {
 }
 
 createBoard();
+
+function click(square) {
+	let currentId = square.id;
+
+	if (isGameOver) {
+		return;
+	}
+	if (square.classList.contains('checked') || square.classList.contains('flag')) {
+		return;
+	}
+	if (square.classList.contains('bomb')) {
+		gameOver(square);
+	} else {
+		let total = square.getAttribute('data');
+		if (total != 0) {
+			square.classList.add('checked');
+			if (total == 1) {
+				square.classList.add('one');
+			}
+			if (total == 2) {
+				square.classList.add('two');
+			}
+			if (total == 3) {
+				square.classList.add('three');
+			}
+			if (total == 4) {
+				square.classList.add('four');
+			}
+			square.innerHTML = total;
+			return;
+		}
+		checkSquare(square, currentId);
+	}
+	square.classList.add('checked');
+}
